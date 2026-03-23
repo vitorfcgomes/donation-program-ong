ff402825ee616b8d2250ab079e81feda156a257b
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-03-23 15:29:57 -0300

    fix global exception handler

--- donation-service/src/main/java/com/doacaoong/donation_service/exceptions/GlobalExceptionHandler.java
+++ donation-service/src/main/java/com/doacaoong/donation_service/exceptions/GlobalExceptionHandler.java
@@ -3,10 +3,12 @@ package com.doacaoong.donation_service.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 import java.util.Map;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)// a exceção em si
     @ResponseStatus(HttpStatus.NOT_FOUND)//o tipo da exception http que irá retornar
